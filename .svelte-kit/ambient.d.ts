7ad49229ea2ab1725206c27c414338131e084637
maximoaparicio <maxcodingg@gmail.com>
2023-10-31 19:05:16 -0300

    reform

--- .svelte-kit/ambient.d.ts
+++ .svelte-kit/ambient.d.ts
@@ -88,7 +88,6 @@ declare module '$env/static/private' {
 	export const PROMPT: string;
 	export const PSModulePath: string;
 	export const PUBLIC: string;
-	export const SESSIONNAME: string;
 	export const SystemDrive: string;
 	export const SystemRoot: string;
 	export const TEMP: string;
@@ -196,7 +195,6 @@ declare module '$env/dynamic/private' {
 		PROMPT: string;
 		PSModulePath: string;
 		PUBLIC: string;
-		SESSIONNAME: string;
 		SystemDrive: string;
 		SystemRoot: string;
 		TEMP: string;
--- .svelte-kit/output/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.23a567f6.js","_app/immutable/chunks/scheduler.c358b412.js","_app/immutable/chunks/index.44da1c4d.js"];
-export const stylesheets = ["_app/immutable/assets/0.33c785d1.css"];
-export const fonts = [];
