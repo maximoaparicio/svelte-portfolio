d84bfd75f77e848a046e9b024d3c308b89d90ad5
Maximo Dip Aparicio <maxcodingg@gmail.com>
2023-11-02 15:09:21 -0300

    change type inp

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -103,7 +103,6 @@
             >Who are you
             <input
               class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg -500 -500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:-500 dark:-500"
-              type="nombre"
               name="nombre"
               id="nombre"
               placeholder="Name"
